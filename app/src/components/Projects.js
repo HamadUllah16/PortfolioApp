b98d56ac777d69907ccd8885b72cac29839615d8
hamadullah16 <hamadullah16@gmail.com>
2023-11-02 04:14:46 +0500

    Optimized Project Images

--- app/src/components/Projects.js
+++ app/src/components/Projects.js
@@ -1,13 +1,14 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
-import littleLemon from "../misc/LittleLemonHome.png";
+import littleLemon from "../misc/LittleLemonHome.jpg";
 import littleLemonGif from "../misc/LittleLemon.gif";
-import kensTree from "../misc/Ken's Discount Tree.png"
+import kensTree from "../misc/Ken's Discount Tree.jpg"
+import demolition from "../misc/demolition.jpg"
 const projects = [
   { name: "Little Lemon Restaurant", description: "Little Lemon a Mediterranean restaurant, focused on traditional receipes served with modern twist.", image: littleLemon, gif: littleLemonGif, github: "https://github.com/HamadUllah16/LittleLemon-Reservations", live: "https://littlelemonchicago.vercel.app", technologies: ["React", "Bootstrap5", "CSS", "UI/UX"] },
 
   { name: "Ken's Discount Tree", description: "Affordable tree services in Atlanta.", image: kensTree, gif: null, github: null, live: "https://www.kensdiscounttreeservice.com", technologies: ["WordPress", "WordPress Customization"] },
-  { name: "Demolition & Hauling", description: "Family owned and operated, demolition and hauling for less has earned the reputation for providing top quality residential and commercial building demolition services to clients throughout Atlanta.", image: kensTree, gif: null, github: null, live: "https://www.kensdiscounttreeservice.com", technologies: ["WordPress", "WordPress Customization"] }
+  { name: "Demolition & Hauling", description: "Family owned and operated, demolition and hauling for less has earned the reputation for providing top quality residential and commercial building demolition services to clients throughout Atlanta.", image: demolition, gif: null, github: null, live: "https://www.demolitionandhaulingforless.com", technologies: ["WordPress", "WordPress Customization"] }
 ]
 
 
