1ef7e64a5dc57bd649940eb5108385f017772333
hamadullah16 <hamadullah16@gmail.com>
2023-12-06 20:54:31 +0500

    more interactivity, minimized some code

--- app/src/components/Projects.js
+++ app/src/components/Projects.js
@@ -9,47 +9,45 @@ const projects = [
   { name: "Little Lemon Restaurant", description: "Little Lemon a Mediterranean restaurant, focused on traditional receipes served with modern twist.", image: littleLemon, gif: littleLemonGif, github: "https://github.com/HamadUllah16/LittleLemon-Reservations", live: "https://littlelemonchicago.vercel.app", technologies: ["React", "Bootstrap5", "CSS", "UI/UX"] },
 
   { name: "Ken's Discount Tree", description: "Affordable tree services in Atlanta.", image: kensTree, gif: null, github: null, live: "https://www.kensdiscounttreeservice.com", technologies: ["WordPress", "WordPress Customization"] },
-  { name: "Demolition & Hauling", description: "Family owned and operated, demolition and hauling for less has earned the reputation for providing top quality residential and commercial building demolition services to clients throughout Atlanta.", image: demolition, gif: null, github: null, live: "https://www.demolitionandhaulingforless.com", technologies: ["WordPress", "WordPress Customization"] }
+  { name: "Demolition & Hauling", description: "Family owned and operated, demolition and hauling for less has earned the reputation for providing top quality residential and commercial building demolition services to clients throughout Atlanta.", image: demolition, gif: null, github: null, live: "https://www.demolitionandhaulingforless.com", technologies: ["WordPress", "WordPress Customization"] },
+  { name: "Yelp Camps", description: "A MERN Stack App with CRUD functionalities", image: "", gif: null, github: "https://yelpcamps-client.vercel.app", live: "https://yelpcamps-client.vercel.app", technologies: ["React", "Express", "NodeJs", "MongoDB", "Bootstrap5"] }
 ]
 
 
 function Projects() {
   return (
-    <section className='row m-2 justify-content-sm-center justify-content-center gap-3'>
+    <section className='projectRow row m-2 justify-content-sm-center justify-content-center gap-3'>
       {projects.map((item, index) => {
         return (
-          <div key={index} className="card shadow cardSize p-0 m-0 col-12 col-md-12 col-lg-6">
+          <a href={item.live} key={index} target='_blank' rel='noreferrer' className="card cardSize p-0 m-0 col-12 col-md-12 col-lg-6">
             <img src={item.image} alt='project-cover' className="card-img-top projectImage"
             />
             <div className="card-body">
-              {/* <a href={item.live} target='_blank' rel='noreferrer'> */}
 
-                <h5 className="card-title navText d-flex justify-content-between align-items-center gap-2"><p className='fs-6 p-0 m-0'>{item.name}</p>
-                {/* <div> <FontAwesomeIcon href={item.github} icon={faGithub} className='gitRepo' size='xs' /> <FontAwesomeIcon icon={faArrowUpRightFromSquare} size="2xs" className='linkIcon' /></div> */}
-                </h5>
-                <p className="card-text ralewayFont description">{item.description}</p>
-                <div className='d-flex gap-3'>
+              <div className="card-title navText d-flex justify-content-between align-items-center gap-2"><p className='fs-6 p-0 m-0'>{item.name}</p>
+                <div className='d-flex align-items-center gap-2'>
                   {item.github ?
-                    <a href={item.github} className='projectLink'><p className='signikaFont'>GitHub <FontAwesomeIcon className='linkIcon' icon={faArrowUpRightFromSquare} size='xs' /></p></a>
+                    <a href={item.github} className='signikaFont m-0'><FontAwesomeIcon icon={faGithub} /></a>
                     :
                     ""
                   }
                   {item.live ?
-                    <a href={item.live} className='projectLink'><p className='signikaFont'>Live <FontAwesomeIcon className='linkIcon' icon={faArrowUpRightFromSquare} size='xs' /></p></a>
+                    <p className='signikaFont m-0'><FontAwesomeIcon className='linkIcon' icon={faArrowUpRightFromSquare} size='xs' /></p>
                     :
                     ""
                   }
                 </div>
-                <article className='d-flex flex-wrap align-items-center'>
-                  {item.technologies.map((item, index) => {
-                    return (
-                      <div key={index} className='techBox'><p className='px-2 text-center m-0'>{item}</p></div>
-                    )
-                  })}
-                </article>
-              {/* </a> */}
+              </div>
+              <p className="card-text ralewayFont description">{item.description}</p>
+              <article className='d-flex flex-wrap align-items-center'>
+                {item.technologies.map((item, index) => {
+                  return (
+                    <div key={index} className='techBox'><p className='px-2 text-center m-0'>{item}</p></div>
+                  )
+                })}
+              </article>
             </div>
-          </div>
+          </a>
         )
       })}
 
--- app/src/components/TechStack.js
+++ app/src/components/TechStack.js
@@ -1,38 +1,40 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHtml5, faCss3, faReact, faJs, faBootstrap, faGithub, faNodeJs, faPython, faJava, faFigma, faAndroid, faGit } from '@fortawesome/free-brands-svg-icons'
-
+import { faHtml5, faCss3, faReact, faJs, faBootstrap, faGithub, faNodeJs, faPython, faJava, faFigma, faAndroid, faGit, faWordpress } from '@fortawesome/free-brands-svg-icons'
+import express from "../misc/express.png"
 const techStack = [
-  { name: "React", icon: faReact },
-  { name: "Bootstrap5", icon: faBootstrap },
-  { name: "NodeJs", icon: faNodeJs },
-  { name: "Javascript", icon: faJs },
-  { name: "Figma", icon: faFigma },
-  { name: "HTML5", icon: faHtml5 },
-  { name: "CSS3", icon: faCss3 },
-  { name: "Python", icon: faPython },
-  { name: "Java", icon: faJava },
-  { name: "Android", icon: faAndroid },
-  { name: "GitHub", icon: faGithub },
-  { name: "Git", icon: faGit }
+  { name: "React", icon: faReact, custom: false },
+  { name: "Bootstrap5", icon: faBootstrap, custom: false },
+  { name: "NodeJs", icon: faNodeJs, custom: false },
+  { name: "Express", icon: express, custom: true },
+  { name: "Javascript", icon: faJs, custom: false },
+  { name: "Figma", icon: faFigma, custom: false },
+  { name: "HTML5", icon: faHtml5, custom: false },
+  { name: "CSS3", icon: faCss3, custom: false },
+  { name: "Python", icon: faPython, custom: false },
+  { name: "Java", icon: faJava, custom: false },
+  { name: "Android", icon: faAndroid, custom: false },
+  { name: "GitHub", icon: faGithub, custom: false },
+  { name: "Git", icon: faGit, custom: false },
+  { name: "WordPress", icon: faWordpress, custom: false }
 ]
 
 function TechStack() {
   return (
     <section className='row mx-lg-4'>
-      <section className='col-12 col-lg-6 col-md-6 col-sm-12 m-3'>
+      <section className='col-12 col-lg-6 col-md-6 col-sm-12 mt-2'>
         <p className='navText fs-4'>Some technologies I've worked with:</p>
-        <article className='row gap-3'>
+        <article className='row gap-3 m-1'>
           {techStack.map((item, index) => {
             return (
-              <article className='col-3 col-sm-3 col-lg-3 col-md-3 text-center' key={index}>
-                <FontAwesomeIcon icon={item.icon} size="4x" />
+              <article className='col-3 col-sm-3 col-lg-2 col-md-3 text-center' key={index}>
+                {item.custom ? <img src={item.icon} style={{maxWidth: "60px"}} /> : <FontAwesomeIcon icon={item.icon} size="4x" />}
               </article>
             )
           })}
         </article>
 
-        <section className='col-12 col-lg-6 col-md-6 col-sm-12 m-3'>
+        <section className='col-12 col-lg-6 col-md-6 col-sm-12 mt-2'>
 
         </section>
       </section>
